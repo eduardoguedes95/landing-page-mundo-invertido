63f4f70c5b8f7628004198552e5b35ca898fcf19
Guedes <eduardoguedes.tec95@outlook.com>
2022-08-26 15:37:43 -0300

    Landing page mundo invertido

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,20 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAdbrcyX9di96H-9adX8tPulHg4fMa1byM",
+    authDomain: "ldpg-mundo-invertido.firebaseapp.com",
+    projectId: "ldpg-mundo-invertido",
+    storageBucket: "ldpg-mundo-invertido.appspot.com",
+    messagingSenderId: "147658753627",
+    appId: "1:147658753627:web:ebfa1f3b6c5ae0f6b8a17e"
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app
